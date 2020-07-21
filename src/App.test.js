bc816083e920536e2aec6c4641865cf5a31f956b
Jeffe <j.garner200@gmail.com>
2020-07-21 16:30:35 -0400

    pre-ejection

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,28 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+
+import App from "./App";
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+it("renders without error", () => {
+	const wrapper = shallow(<App />);
+	const appComponent = wrapper.find("[data-test='component-app']");
+
+	expect(appComponent.length).toBe(1);
 });
+
+// it("renders increment button", () => {
+// 	const wrapper = shallow(<App />);
+// 	const buttonEle = wrapper.find("[data-test='button']");
+
+// 	expect(buttonEle.length).toBe(1);
+// });
+
+// it("renders counter display", () => {});
+
+// it("begins with the counter at 0", () => {});
+
+// it("clicking button increments counter display by one", () => {});
